62c976045d67575f0040082e7c02666fe71877e0
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-07-22 22:05:59 +0530

    animationImprove

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,133 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faDownload } from "@fortawesome/free-solid-svg-icons";
+
+const menuVariants = {
+  hidden: { y: "-100%", opacity: 0 },
+  visible: { y: 0, opacity: 1, duration: 1 },
+  left: { x: -150, opacity: 0 },
+  right: { x: 150, opacity: 0 },
+};
+
+const hamburgerVariants = {
+  open: { rotate: 90 },
+  closed: { rotate: 0 },
+};
+
+const iconVariants = {
+  rotating: {
+    rotate: 360,
+    transition: {
+      duration: 0.4,
+      ease: "linear",
+    },
+  },
+};
 
 function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const hamBurgerRef = useRef(null);
+  const mobileMenuListRef = useRef(null);
+  const [isIconRotating, setIconRotating] = useState(false); // State to manage the rotation animation
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      mobileMenuListRef.current &&
+      !mobileMenuListRef.current.contains(event.target) &&
+      event.target !== hamBurgerRef.current
+    ) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    if (isIconRotating) {
+      setTimeout(() => {
+        setIconRotating(false);
+      }, 1000); // Wait for 1 second (duration of the animation) and then reset the rotation state
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isMenuOpen, isIconRotating]);
+
+  const handleDownloadResume = () => {
+    setIconRotating(true);
+    const resumeFilePath = "/images/LazimLatheef.pdf";
+    const link = document.createElement("a");
+    link.href = resumeFilePath;
+    link.download = "MohammedLazimLatheef.pdf";
+    link.click();
+  };
+
   return (
-    <motion.div
-      className="headerList container flex justify-around items-center h-20 shadow-lg shadow-blue-950"
-      initial={{ opacity: 0, y: -200 }}
-      animate={{ opacity: 1, y: 0 }}
-      exist={{ opacity: 0, y: -200 }}
-      transition={{ delay: 0.1, duration: 0.55 }}
-    >
-      <h6 className="text-2xl hover:text-sky-700 cursor-pointer">HOME</h6>
-      <h6 className="text-2xl hover:text-sky-700 cursor-pointer">PROJECT</h6>
-      <h6 className="text-2xl hover:text-sky-700 cursor-pointer">ABOUT</h6>
-      <h6 className="text-2xl hover:text-sky-700 cursor-pointer">CONTACT</h6>
-    </motion.div>
+    <>
+      <motion.div
+        className="headerList container flex justify-around items-center h-20 shadow-lg shadow-blue-950"
+        initial={{ opacity: 0, y: -200 }}
+        animate={{ opacity: 1, y: 0 }}
+        exist={{ opacity: 0, y: -200 }}
+        transition={{ delay: 0.1, duration: 0.55 }}
+      >
+        <h6 className="text-2xl hover:text-sky-700 cursor-pointer">HOME</h6>
+        <h6 className="text-2xl hover:text-sky-700 cursor-pointer">PROJECT</h6>
+        <h6 className="text-2xl hover:text-sky-700 cursor-pointer">ABOUT</h6>
+        <h6 className="text-2xl hover:text-sky-700 cursor-pointer">CONTACT</h6>
+      </motion.div>
+      <div className="mobileHeader container flex items-center h-20 shadow-lg shadow-blue-950">
+        <div className="resume">
+          <p className="text-4xl">Resume</p>
+          <motion.div
+            className="iconDownload"
+            onClick={handleDownloadResume}
+            animate={isIconRotating ? "rotating" : "static"}
+            variants={iconVariants}
+          >
+            <FontAwesomeIcon icon={faDownload} />
+          </motion.div>
+        </div>
+        <div className="hamBurgerContainer">
+          <motion.div
+            className="hamBurger"
+            onClick={toggleMenu}
+            ref={hamBurgerRef}
+            variants={hamburgerVariants}
+            initial="closed"
+            animate={isMenuOpen ? "open" : "closed"}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </motion.div>
+          <motion.div
+            className="mobileMenuList"
+            style={{ display: isMenuOpen ? "flex" : "none" }}
+            ref={mobileMenuListRef}
+            variants={menuVariants}
+            initial="hidden"
+            animate={isMenuOpen ? "visible" : "hidden"}
+            exit="hidden"
+            transition={{ duration: 0.4 }}
+          >
+            <h6 className="text-3xl">HOME</h6>
+            <h6 className="text-3xl">PROJECT</h6>
+            <h6 className="text-3xl">ABOUT</h6>
+            <h6 className="text-3xl">CONTACT</h6>
+          </motion.div>
+        </div>
+      </div>
+    </>
   );
 }
 
